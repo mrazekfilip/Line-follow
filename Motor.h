a9794141a200086c488556ef499741648c0a5006
Filip Mrazek <email@filipmrazek.cz>
2017-06-18 21:54:05 +0200

    motor pid regulation

--- Motor.h
+++ Motor.h
@@ -9,6 +9,7 @@ private:
     Encoder enc;
     PID pid;
     void sendSpeed();
+    double P, I, D;
 public:
     Motor(int pinForward, int pinBackward, int pinEncoderCS, float radius);
     void setSpeed(double absoluteSpeed);
@@ -18,9 +19,12 @@ public:
 };
 
 Motor::Motor(int pinForward, int pinBackward, int pinEncoderCS, float radius) :
-        pinMotorF(pinForward), pinMotorB(pinBackward), enc(pinEncoderCS, radius), pid(&enc.speed, &speed, &absoluteSpeed, 2, 5, 1, DIRECT) {
+        pinMotorF(pinForward), pinMotorB(pinBackward), enc(pinEncoderCS, radius), pid(&enc.speed, &speed, &absoluteSpeed, 1.5, 14, 0.1, DIRECT) {
     this->absoluteSpeed = 0;
     this->speed = 0;
+    this->P = 1.5;
+    this->I = 14;
+    this->D = 0.1;
     pid.SetMode(AUTOMATIC);
 }
 
@@ -55,9 +59,54 @@ double Motor::getSpeed(){
 }
 
 void Motor::setSpeed(double absoluteSpeed){
+    char keystroke = ' ';
     this->absoluteSpeed = absoluteSpeed;
+
+    if (Serial.available()>0){
+        keystroke = Serial.read();
+
+        switch(keystroke){
+            case 'w':
+            this->P += 0.1;
+            break;
+            case 's':
+            this->P -= 0.1;
+            break;
+            case 'e':
+            this->I += 0.1;
+            break;
+            case 'd':
+            this->I -= 0.1;
+            break;
+            case 'r':
+            this->D += 0.1;
+            break;
+            case 'f':
+            this->D -= 0.1;
+            break;
+        }
+        pid.SetTunings(P, I, D);
+    }
+
+    static int i = 0;
+
+    if (i == 100){
+        i = 0;
+        Serial.print(pid.GetKp());
+        Serial.print(" ");
+        Serial.print(pid.GetKi());
+        Serial.print(" ");
+        Serial.print(pid.GetKd());
+        Serial.print(" Output:");
+        Serial.print(getSpeed());
+        Serial.print(" Speed:");
+        Serial.print(enc.speed);
+        Serial.print("\n");
+    }
+    i++;
+
+
     enc.readDistance();
-    enc.printDistance();
     pid.Compute();
     sendSpeed();
 }
--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 #include"Motor.h"
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(57600);
     Serial.println("start");
 
     Motor motorL(6, 11, 9, 1.15);
 
     while(true) {
-        motorL.setSpeed(20);
-        Serial.println(motorL.getSpeed());
-        delay(10);
+        motorL.setSpeed(35);
+
+        delay(5);
     }
 }
 
